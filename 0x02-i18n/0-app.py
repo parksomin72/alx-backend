fc82466279cc9059c966595b6822ce513426796b
parksomin72 <fatimazahraealawi@gmail.com>
2024-05-14 22:13:04 +0000

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+0-app module - Basic Flask app setup
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Render index.html template."""
+    return render_template('0-index.html', title='Welcome to Holberton', header='Hello world')
+
+
+if __name__ == '__main__':
+    app.run()
