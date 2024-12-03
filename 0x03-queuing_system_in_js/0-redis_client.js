33635f9a5b930af52e6b2f7e35b2139e1b118e82
parksomin72 <fatimazahraealawi@gmail.com>
2024-12-03 13:56:52 +0000

    Install a redis instance

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,12 +1,14 @@
-#!/usr/bin/yarn dev
-import { createClient } from 'redis';
+import redis from 'redis';
 
-const client = createClient();
 
-client.on('error', (err) => {
-  console.log('Redis client not connected to the server:', err.toString());
-});
+const client = redis.createClient();
+
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
+});
+
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
 });
--- 0x03-queuing_system_in_js/README.md
+++ 0x03-queuing_system_in_js/README.md
@@ -1,2 +1,14 @@
 # Queuing System in JS
-This project involves setting up a Redis server and performing basic operations.
+
+## Description
+This project demonstrates a queuing system in JavaScript using Redis and Kue.
+
+## Prerequisites
+- Ubuntu 18.04
+- Node.js 12.x
+- Redis 5.0.7 or higher
+
+## Setup
+1. Install dependencies:
+   ```bash
+   npm install
